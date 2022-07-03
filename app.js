bc8091220a06c1196c1bd429aa78572efb99b5c8
unknown <n.andyvue@outlook.com>
2022-07-02 23:47:12 -0500

    pause

--- app.js
+++ app.js
@@ -1,10 +1,11 @@
-const fs = require('fs');
-const generatePage = require('./src/page-template.js');
+const inquirer = require('inquirer')
+// const fs = require('fs');
+// const generatePage = require('./src/page-template.js');
 
-const profileDataArgs = process.argv.slice(2);
+// const pageHTML = generatePage(name, github);
 
-fs.writeFile('index.html', generatePage(name, github), err => {
-  if (err) throw err;
+// fs.writeFile('index.html', pageHTML, err => {
+//   if (err) throw err;
 
-  console.log('Portfolio complete! Check out index.html to see output!');
-});
+//   console.log('Portfolio complete! Check out index.html to see output!');
+// });
